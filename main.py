1b493b4b72f2bb317fa78e6cc7865b4223975e95
Matthew <58238450+Weezity@users.noreply.github.com>
2020-03-31 22:56:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import requests
 import re
 import csv
 
-
-csv_file = open('shoes.csv','w')
+link = "Your link here"
+csv_file = open('output.csv','w')
 csv_writer = csv.writer(csv_file)
 
 def kijiji(keyss):
-    source = requests.get('https://www.kijiji.ca/b-mens-shoes/markham-york-region/page-'+keyss+'/c15117001l1700274').text
+    source = requests.get(link +keyss+'/c15117001l1700274').text
     soup = BeautifulSoup(source,'lxml')
     b = soup.find('div', class_='price')
     for link in soup.find_all('a',class_ = 'title'):
